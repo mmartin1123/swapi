7f3d7e360d3cbc3dfb20d29b124573953361fb45
Michael Martin <mictin3@gmail.com>
2019-07-21 13:15:01 -0600

    Added a few lines of JS

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('success');
+async function getSwapiAPI() {
+    const resp = await fetch('https://swapi.co/api/people/2/');
+    const data = await resp.json();
+    console.log(data);
+
+    const body = document.body
+    console.log(body);
+    body.innerHTML = 'Name: ' + data.name + '<br>' + 'Height: ' + data.height;
+}
+getSwapiAPI();
